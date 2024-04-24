c86f0ea02802af11297101ace4c6fd92b65f4b88
Chiara Castiglioni <chiara.castiglioni@studio.unibo.it>
2024-04-24 14:37:54 +0200

    Delete wrong things

--- river-monitoring-dashboard/WebApp.js
+++ river-monitoring-dashboard/WebApp.js
@@ -1,10 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function () {
-    initializeChart();
-    // Supponiamo che updateWaterLevel venga chiamata periodicamente con nuovi livelli
-}); */
-
-//let chart = null; // Variabile globale per mantenere il riferimento al grafico
-
 let t = setInterval(getData, 2000);
 
 let dataRecived = { 'Sonar': " ", 'Valve': " ", 'State': " " }
@@ -30,87 +23,13 @@ function getData() {
     };
     xhr.send();
 }
-/**
-let waterLevels = []; // Array per memorizzare i livelli dell'acqua
-let maxDataPoints = 20000; // Sostituisci N con il numero di minuti che vuoi visualizzare
-
-function updateWaterLevel(newLevel) {
-    const now = new Date(); // Data e ora corrente
-
-    waterLevels.push({
-        time: now,
-        level: newLevel
-    });
-
-    // Mantieni solo gli ultimi N punti dati
-    while (waterLevels.length > maxDataPoints) {
-        waterLevels.shift();
-    }
-
-    // Aggiorna il grafico
-    updateChart();
-}
-
-
-function initializeChart() {
-    const ctx = document.getElementById('waterLevelChart').getContext('2d');
-    chart = new Chart(ctx, {
-        type: 'line', // Tipo di grafico: linea
-        data: {
-            datasets: [{
-                label: 'Livello dell\'Acqua',
-                backgroundColor: 'rgba(54, 162, 235, 0.5)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                data: [] // Dati iniziali vuoti
-            }]
-        },
-        options: {
-            scales: {
-                x: [{
-                    type: 'time',
-                    time: {
-                        unit: 'minute',
-                        tooltipFormat: 'HH:mm'
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Orario'
-                    }
-                }],
-                y: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Livello dell\'acqua'
-                    }
-                }]
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            }
-        }
-    });
-}
-
-function updateChart() {
-    if (chart) {
-        chart.data.labels = waterLevels.map(data => data.time);
-        chart.data.datasets[0].data = waterLevels.map(data => data.level);
-        chart.update();
-    }
-} */
-
 
 /*Funzione per cambiare i valori delle label */
 function changeLabel() {
     let sonar = document.getElementById("WaterLevel");
     let valve = document.getElementById("ValveLevel");
     let state = document.getElementById("Stato");
-    //updateWaterLevel(dataRecived.Sonar);
+
 
     sonar.innerText = "Sonar level: " + dataRecived.Sonar;
     valve.innerText = "Water level: " + dataRecived.Valve;
@@ -137,21 +56,21 @@ document.getElementById("Submit").addEventListener("click", function () {
     let url = "http://localhost:8080";
     xhr.open('POST', url);
     xhr.setRequestHeader("Content-Type", "application/json");
-    
-    xhr.send(JSON.stringify({'value': inputValue}));
+
+    xhr.send(JSON.stringify({ 'value': inputValue }));
     // Gestisce la risposta dal server
     xhr.onload = function () {
-        if(xhr.status === 201) {
-            console.log("Post successfully created!") 
+        if (xhr.status === 201) {
+            console.log("Post successfully created!")
         }
     }
 });
 
 let slider = document.getElementById("inputValue");
-    var output = document.getElementById("sliderValue");
-    output.innerHTML = slider.value; // Mostra il valore iniziale del cursore
+var output = document.getElementById("sliderValue");
+output.innerHTML = slider.value; // Mostra il valore iniziale del cursore
 
-    // Aggiorna il testo dello span ogni volta che il valore del cursore cambia
-    slider.oninput = function() {
-        output.innerHTML = this.value;
-    }
+// Aggiorna il testo dello span ogni volta che il valore del cursore cambia
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
